0084889cacb556397dab86fb0a0dd0a4a47510ce
Edgar Zea <cesszea@icloud.com>
2023-02-28 23:40:01 -0600

    feat(clicked): change button classes on click

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames';
 import { ButtonVariants, ButtonSizes } from '@/ts';
 import Image from 'next/image';
+import { useState } from 'react';
 
 interface ButtonProps {
   content: string | JSX.Element;
@@ -9,17 +10,27 @@ interface ButtonProps {
 }
 
 const Button: React.FC<ButtonProps> = ({ content, size, variant = 'primary' }) => {
+  const [hasBeenCopied, setHasBeenCopied] = useState(false);
+
   const classes = classNames(`btn btn-${variant} font-georgia`, {
     'btn-sm': size === 'md',
     'btn-lg': size === 'lg',
-    'text-white': !['white', 'code'].includes(variant)
+    'text-white': !['white', 'code'].includes(variant),
+    'btn-code-copied': hasBeenCopied && variant === 'code',
   });
+
+  const handleCopy = () => {
+    if (!hasBeenCopied) {
+      setHasBeenCopied(true);
+    }
+  };
 
   return (
     <div className='d-grid'>
       <button
         className={classes}
         type='button'
+        onClick={handleCopy}
       >
         {content}
         {variant === 'code' && (
