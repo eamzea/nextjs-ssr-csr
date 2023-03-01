19b47bd656de5a3161223cf6cd0d7cdc8542f8f1
Edgar Zea <cesszea@icloud.com>
2023-02-28 23:31:56 -0600

    feat(fetch): get elements

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -8,6 +8,7 @@ export type ButtonVariants =
   | 'danger'
   | 'black'
   | 'white'
+  | 'code'
   | 'outline-primary'
   | 'outline-primary-light'
   | 'outline-primary-dark'
@@ -19,3 +20,66 @@ export type ButtonVariants =
   | 'outline-white';
 
 export type ButtonSizes = 'md' | 'lg';
+
+export interface Response {
+  height: number;
+  width: number;
+  externalUrl: string;
+  ctcommercial_offer_name: string;
+  ctcreative_name: string;
+  ctaffiliate_program: string;
+  ctaffiliate_link: string;
+  ctap_account: string;
+  ctproduct_name: string;
+  ctproduct_group: string;
+  ctproduct_type: string;
+  ctstart_date: string;
+  ctend_date: string;
+  ctoriginal_start_date: string;
+  ctbrand_name: string;
+  ctcurrency: string;
+  ctoffer_tc: string;
+  cttc_link: string;
+  ctavailable_only_for_mobile: string;
+  ctrestrictions: string;
+  ctmarketing_information: string;
+  ctoffer_type: string;
+  ctcash_out: string;
+  ctbonus_type: string;
+  ctbonus_amount: string;
+  ctbonus_percentage: string;
+  ctminimum_deposit: string;
+  ctminimum_stake: string;
+  ctlive_streaming_service: string;
+  ctqualifying_bet: string;
+  ctpromo_code: string;
+  ctopt_in_required: string;
+  ctwagering_requirements: string;
+  ctmax_free_bet: string;
+  ctmax_refund: string;
+  ctwhich_race: string;
+  ctmax_extra_places: string;
+  ctmin_place_terms: string;
+  ctmin_runners: string;
+  ctselected_races_only: string;
+  ctmMinimum_bet_per_spin: string;
+  ctmaximum_bet_per_spin: string;
+  ctfree_spins_amount: string;
+  ctfree_spins_valid_for: string;
+  cttournament_tokens_amount: string;
+  ctmax_winnings: string;
+  ctbonus_cap: string;
+  ctminimum_bingo_tickets: string;
+  ctmaximum_bingo_tickets: string;
+  ctbingo_variants: string;
+  ctbingo_rooms: string;
+  ctminimum_lottery_tickets: string;
+  ctmaximum_lottery_tickets: string;
+  ctlotto_free_bets: string;
+  ctlotto_variants: string;
+  ctpoker_variant: string;
+  cttournaments_type: string;
+  ctoffer_brand_features: string;
+  cttournament_info: string;
+  ctminimum_odds: string;
+}
